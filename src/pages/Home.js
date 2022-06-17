ba267fef17a30d8e605ccb7d8137349bb45748bb
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2022-06-17 15:48:31 +0800

    Finish Block Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import ReactSplit, { SplitDirection } from '@devbookhq/splitter'
+import FloatingLabel from "react-bootstrap-floating-label";
+import "./Home.css"
 
 function Home() {
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home">
+            <div className = "split-pane">
+                <ReactSplit  direction={SplitDirection.Horizontal}>
+                    <div className="left-side-content">
+                        <div className="form-container">
+                            <h2> Add Block </h2>
+                            
+                            <FloatingLabel label="Header " id='header' className= 'floating-label'/>
+                            <FloatingLabel label="Author " id='author' className= 'floating-label'/>
+                            <FloatingLabel label="Parent Hash " id='hash' className= 'floating-label'/>
+                            <FloatingLabel label="Content " id='content' className= 'floating-label'/>
+
+                            <button className="add-block-btn"><span>Add Block</span></button>
+
+                        </div>
+                    </div>
+
+                    <div className="right-side-content">
+                        <div className="block-container">
+                            <label className="block-header">Block Header</label>
+                            <label className="block-author">Block Author</label>
+                            <label className="block-content">Block Content</label>
+                            <label className="block-author">Data address</label>
+                            <label className="block-hash">Block Hash</label>
+                            <label className="block-hash">Public Key</label>
+                            <label className="block-hash">Signature </label>
+                            <label className="block-hash">Parent Hash</label>
+                            <label className="block-hash">Timestamp</label>
+                        </div>
+                    </div>
+                </ReactSplit>
+            </div>
         </div>
     );
 }
