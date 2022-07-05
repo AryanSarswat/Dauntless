f0d63cab0db84023401073321db81c8bd52d88fe
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2022-07-05 10:17:15 +0800

    Fix list error by adding key and id to elements in list

--- src/pages/BlockPage.js
+++ src/pages/BlockPage.js
@@ -7,14 +7,14 @@ function BlockPage(props) {
         <div className="block-page-container">
             <h1> Blocks </h1>
             {props.blockchain.getAllBlocks().map(block => {
-                return <Collapse header={block.header} id={block.hash}>
-                    <ul>
-                        <li className='collapsible-label'>Hash: {block.hash}</li>
-                        <li className='collapsible-label'>Nonce: {block.nonce}</li>
-                        <li className='collapsible-label'>Timestamp: {block.time}</li>
-                        {block.parentHash && <li className='collapsible-label'>Parent Hash: {block.parentHash}</li>}
-                        <li className='collapsible-label'>Data Address: {block.dataAddress}</li>
-                    </ul>
+                return <Collapse header={block.header} id={block.hash} key={block.hash}>
+                            <ul>
+                                <li  id={block.hash} key={block.hash} className='collapsible-label'>Hash: {block.hash}</li>
+                                <li id={block.nonce} key={block.nonce} className='collapsible-label'>Nonce: {block.nonce}</li>
+                                <li id={block.time} key={block.time} className='collapsible-label'>Timestamp: {block.time}</li>
+                                {block.parentHash && <li id={block.parentHash} key={block.parentHash} className='collapsible-label'>Parent Hash: {block.parentHash}</li>}
+                                <li id={block.dataAddress} key={block.dataAddress} className='collapsible-label'>Data Address: {block.dataAddress}</li>
+                            </ul>
                         </Collapse>
             })}
         </div>
