4800e8951fcac15fbc39383b69af156bb597fb1e
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2022-07-12 13:38:13 +0800

    Clean up IPFS.py

--- server/model/IPFS.py
+++ server/model/IPFS.py
@@ -6,14 +6,24 @@ class IPFS:
     def __init__(self) -> None:
         self.storage = dict()
     
-    def saveData(self, filename):
-        filename = os.path.join(filename, "IPFS.json")
+    def saveData(self, filepath):
+        """saves the data in the IPFS to a file called IPFS.json
+
+        Args:
+            filepath (str): path to the directory for saving the IPFS.json file
+        """
+        filename = os.path.join(filepath, "IPFS.json")
         with open(filename, 'w') as file:
             toWrite = json.dumps(self.storage, default= lambda x: x.hex(),indent = 4)
             file.write(toWrite)
     
-    def loadData(self, filename):
-        filename = os.path.join(filename, "IPFS.json")
+    def loadData(self, filepath):
+        """Loads the data from the IPFS.json file
+
+        Args:
+            filename (_type_): path to the directory for loading the IPFS.json file
+        """
+        filename = os.path.join(filepath, "IPFS.json")
         try:
             with open(filename, 'r') as file:
                 file_content = file.read()
@@ -27,13 +37,30 @@ class IPFS:
             print("No IPFS data found")
             return dict()
             
-    def getText(self, hash):
+    def getText(self, hash: str) -> str:
+        """Retrieves the stored information from the given hash
+
+        Args:
+            hash (str): hash of the text to retrieve
+
+        Returns:
+            str: text stored at the given hash
+        """
         try:
             return self.storage[hash]
         except KeyError:
             return "No text matching the given hash found"
     
-    def store(self, text, type):
+    def store(self, text: str, type: str) -> str:
+        """Hashes the given text and stores it in the IPFS
+
+        Args:
+            text (str): text to store in the IPFS
+            type (str): type of the text to store in the IPFS
+
+        Returns:
+            hash: hash of the text stored in the IPFS
+        """
         temp_hash = hashlib.sha256()
         # Hash of block is computed using the following properties
         temp_hash.update(text.encode('utf-8'))
@@ -42,12 +69,28 @@ class IPFS:
         return hash
     
     def isInIPFS(self, text):
+        """Checks if the given text is in the IPFS
+
+        Args:
+            text (str): text to check if it is in the IPFS
+
+        Returns:
+            bool: True if the text is in the IPFS, False otherwise
+        """
         for value, type in self.storage.values():
             if value == text:
                 return True
         return False
     
     def getDataAddress(self, text):
+        """Returns the data address of the given text
+
+        Args:
+            text (str): text to get the data address of
+
+        Returns:
+            dataAddress (str): data address of the given text
+        """
         for key, value in self.storage.items():
             if value[0] == text:
                 return key
