f0d5c8510953d19ba7bc9841f36fb7022deeb803
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2022-07-12 12:19:56 +0800

    Clean up blockchain.py

--- server/model/Blockchain.py
+++ server/model/Blockchain.py
@@ -1,21 +1,23 @@
-from __future__ import annotations
-
 import numpy as np
+from __future__ import annotations
 from tqdm import tqdm
 from .Block import *
 from .Graph import *
 
 class BlockIsNotFoundError(Exception):
+    """Error for when a block is not found in the blockchain"""
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
 
-class ParentDoesNotContainCHildError(Exception):
-        def __init__(self):
-            self.message = f"The child block points to the parent block which does not contain the child block"
-            super().__init__(self.message)
+class ParentDoesNotContainChildError(Exception):
+    """Error when a Parent does not contain a child block while a traceback is being done"""
+    def __init__(self):
+        self.message = f"The child block points to the parent block which does not contain the child block"
+        super().__init__(self.message)
 
 class InvalidParentHashError(Exception):
+    """Error for mismatch in Hash of parent and child"""
     def __init__(self, actual_parent_hash, block_parent_hash):
         self.parent_hash = actual_parent_hash
         self.block_parent_hash = block_parent_hash
@@ -44,6 +46,18 @@ class Blockchain:
         self.blockMappings = dict()
 
     def mine(self, parent_block_hash, new_block) -> None:
+        """Mines a new block and adds it to the blockchain
+
+        Args:
+            parent_block_hash (_type_): hash of the parent block
+            new_block (_type_): block to be mined
+
+        Raises:
+            MaxNonceError: when the maximum nonce has been reached
+
+        Returns:
+            block: the mined block
+        """
         new_block.computeHash()
         for n in tqdm(range(self.MAX_NONCE), desc="Mining Block"):
             if self.isValidProof(new_block, new_block.getHash()):
@@ -99,23 +113,39 @@ class Blockchain:
         while block.parent_hash is not None:
             parent_block = self.getBlock(block.parent_hash)
             if block.getHash() not in parent_block.child_hashes:
-                raise ParentDoesNotContainCHildError()
+                raise ParentDoesNotContainChildError()
             block = parent_block
             history.append(block)
         return history[::-1]
     
-    def saveData(self, filename: str) -> None:
+    def saveData(self, filepath: str) -> None:
+        """Saves the blockchain to a file locating in the filepath provided
+
+        Args:
+            filepath (str): path to the directory where the blockchain will be saved
+        """
         listOfBlocks = list(self.blockMappings.values())
-        Block.storeBlocks(listOfBlocks, filename)
+        Block.storeBlocks(listOfBlocks, filepath)
     
     def printBlockGraph(self) -> None:
+        """Prints the block graph"""
         self.graph.printGraph(self.blockMappings)
         
     def saveBlockGraph(self) -> None:
+        """Saves the block graph to a file"""
         self.graph.exportGraph(self.blockMappings)
 
     @staticmethod
     def isValidProof(block, block_hash) -> bool:
+        """Function to check if the proof of work is valid
+
+        Args:
+            block (_type_): block to be checked
+            block_hash (_type_): hash of the block to be checked
+
+        Returns:
+            bool: True if the proof of work is valid, False otherwise
+        """
         block_hash = bytes.fromhex(block_hash)
         return block_hash.startswith(b'0' * Blockchain.DIFFICULTY) and block_hash.hex() == block.computeHash()
     
